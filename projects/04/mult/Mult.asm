b397038d5e9516475f7051bc868b0b618a3b7f18
Peter Polidoro <peterpolidoro@gmail.com>
2019-09-10 17:06:51 -0400

    First attempt at Mult.asm

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,3 +7,47 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+  @R0
+  D=M
+  @n
+  M=D // n = R0
+
+  @R1
+  D=M
+  @m
+  M=D // m = R1
+
+  @i
+  M=0 // i = 0
+
+  @mult
+  M=0 // mult = 0
+
+(LOOP)
+  @i
+  D=M
+  @m
+  D=D-M
+  @STOP
+  D;JEQ
+
+  @mult
+  D=M
+  @n
+  D=D+M
+  @MULT
+  M=D
+  @i
+  M=M+1
+  @LOOP
+  0;JMP
+
+(STOP)
+  @mult
+  D=M
+  @R2
+  M=D // RAM[2] = mult
+
+(END)
+  @END
+  0;JMP
