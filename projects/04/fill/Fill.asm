f4cdeb76e9991675f2cecb3b1ccef4ce3506160c
Peter Polidoro <peterpolidoro@gmail.com>
2019-09-11 15:15:16 -0400

    Fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,51 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(POLLKBD)
+  @KBD
+  D=M
+  @BLACK
+  D;JNE
+  @WHITE
+  0;JMP
+
+(WHITE)
+  @value
+  M=0
+  @FILL
+  0;JMP
+
+(BLACK)
+  @value
+  M=-1
+  @FILL
+  0;JMP
+
+(FILL)
+  @SCREEN
+  D=A
+  @addr
+  M=D
+  @lastaddr
+  M=D
+  @8192
+  D=A
+  @lastaddr
+  M=D+M
+(LOOP)
+  @lastaddr
+  D=M
+  @addr
+  D=D-M
+  @POLLKBD
+  D;JEQ
+  @value
+  D=M
+  @addr
+  A=M
+  M=D
+  @addr
+  M=M+1
+  @LOOP
+  0;JMP
